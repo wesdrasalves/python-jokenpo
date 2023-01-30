0b24983fe9e526d8807508156cf688891b68887f
Wesdras Alves <wesdras.alves@gmail.com>
2023-01-30 08:39:03 -0300

    Primeira versão do Jokenpô usando apenas print e input

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+
+print("Seja bem vindo ao Jokenpô")
+print("Esse jogo exige que você escolha uma das opções abaixo")
+print("Pedra, papel ou tesoura.")
+
+jogador1 = input("Digite sua opção ")
+
+os.system("cls")
+
+print("Seja bem vindo ao Jokenpô")
+print("Digite a sua escolha Jogador 2")
+print("Pedra, papel ou tesoura.")
+
+jogador2 = input("Digite sua opção")
+
+os.system("cls")
+
+print("O jogador 1 escolheu", jogador1, "e o jogador 2 escolheu", jogador2)
+
